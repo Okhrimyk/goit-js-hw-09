33d4408995aad0701b352de1de516df292697f5c
Okhrimyk <ValeriiOkhrimyk@gmail.com>
2025-10-14 23:39:23 +0000

    Deploying to gh-pages from @ Okhrimyk/goit-js-hw-09@2172919445ac448fb83fdb323ccdaea1f93d700b 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,4 +1,4 @@
-import"./assets/styles-BybVkJee.js";const o=document.querySelector(".feedback-form"),r=[`
+import"./assets/styles-BybVkJee.js";const o=document.querySelector(".feedback-form"),l=[`
   <label>
     Email
     <input type="email" name="email" autofocus />
@@ -7,5 +7,5 @@ import"./assets/styles-BybVkJee.js";const o=document.querySelector(".feedback-fo
     Message
     <textarea name="message" rows="8"></textarea>
   </label>
-  <button type="submit">Log in</button>`].join("");o.innerHTML=r;const s=document.querySelector(".feedback-form"),n=s.elements.email,l=s.elements.message,a="feedback-form-state",e={email:"",message:""};n.addEventListener("input",i);l.addEventListener("input",u);s.addEventListener("submit",g);function i(t){e.email=t.target.value,localStorage.setItem(a,JSON.stringify(e))}function u(t){e.message=t.target.value,localStorage.setItem(a,JSON.stringify(e))}c();function c(){const t=localStorage.getItem(a);if(t){const m=JSON.parse(t);n.value=m.email||"",l.value=m.message||"",e.email=parsedData.email||"",e.message=parsedData.message||""}}function g(t){if(t.preventDefault(),!e.email.trim()||!e.message.trim()){alert("Fill please all fields");return}t.target.reset(),localStorage.removeItem(a),e.email="",e.message=""}
+  <button type="submit">Log in</button>`].join("");o.innerHTML=l;const m=document.querySelector(".feedback-form"),n=m.elements.email,r=m.elements.message,s="feedback-form-state",a={email:"",message:""};c();m.addEventListener("input",i);m.addEventListener("submit",u);function i(e){(e.target.name==="email"||e.target.name==="message")&&(a[e.target.name]=e.target.value,localStorage.setItem(s,JSON.stringify(a)))}function c(){const e=localStorage.getItem(s);if(e){const t=JSON.parse(e);n.value=t.email||"",r.value=t.message||"",a.email=t.email||"",a.message=t.message||""}}function u(e){if(e.preventDefault(),!a.email.trim()||!a.message.trim()){alert("Fill please all fields");return}console.log(a),e.target.reset(),localStorage.removeItem(s),a.email="",a.message=""}
 //# sourceMappingURL=2-form.js.map
